2477f8d376c6922b441c38ced97c029bc5972a57
Филипп Фуга <cyhka312@mail.ru>
2025-03-23 13:18:58 +0500

    Add ParseFlag() implement

--- main.go
+++ main.go
@@ -1,14 +1,62 @@
 package main
 
 import (
+	"errors"
+	"flag"
 	"fmt"
+	"os"
+	"strings"
 )
 
-func main() {
-	s := "gopher"
-	fmt.Printf("Hello and welcome, %s!\n", s)
+type Options struct {
+	From      string
+	To        string
+	Offset    uint
+	Limit     int
+	blocksize uint
+	conv      string
+}
 
-	for i := 1; i <= 5; i++ {
-		fmt.Println("i =", 100/i)
+func ParseFlags() (*Options, error) {
+	var opts Options
+
+	flag.StringVar(&opts.From, "from", "", "file to read. by default - stdin")
+	flag.StringVar(&opts.To, "to", "", "file to write. by default - stdout")
+	flag.UintVar(&opts.Offset, "offset", 0, "Number of bytes to skip before copying")
+	flag.IntVar(&opts.Limit, "limit", -1, "Max bytes to read (-1 for all)")
+	flag.UintVar(&opts.blocksize, "blocksize", 4096, "Block size for reading/writing")
+	flag.StringVar(&opts.conv, "conv", "", "upper_case, lower_case, trim_spaces")
+
+	flag.Parse()
+
+	conv := strings.Split(opts.conv, ",")
+	upper := false
+	lower := false
+	for _, str := range conv {
+		str = strings.TrimSpace(str)
+		if str == "" {
+			continue
+		}
+		switch str {
+		case "upper_case":
+			upper = true
+		case "lower_case":
+			lower = true
+		}
 	}
+	if upper && lower {
+		return nil, errors.New("cannot use both upper_case and lower_case")
+	}
+	return &opts, nil
+
+}
+
+func main() {
+	opts, err := ParseFlags()
+	if err != nil {
+		_, _ = fmt.Fprintln(os.Stderr, "can not parse flags:", err)
+		os.Exit(1)
+	}
+
+	fmt.Println(*opts)
 }
